3c88da859c2f9ce665bfc5423c58a271e9e35c11
@ <yuezhaoyang7449@163.com>
2020-04-08 14:36:39 +0800

    add cn-wps

--- Casks/cn-wps.rb
+++ Casks/cn-wps.rb
@@ -1,9 +1,9 @@
 cask 'cn-wps' do
-  version '70459'
+  version '2.1.2'
   sha256 '9f560401cbaab011b7edfed119f2b8230422ddccea5d6d37c9d6101c53b04d6d'
 
   # cn-wps was verified as official when first introduced to the cask
-  url "http://download.taobaocdn.com/freedom/#{version}/default/Alimail.dmg"
+  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version}/WPS_Office_#{version}(3417).dmg"
   name 'cn-wps'
   homepage 'https://www.wps.cn/'
 
