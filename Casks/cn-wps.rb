d873c710884dfa63fce63278e225e298fb49c9bf
@ <yuezhaoyang7449@163.com>
2020-08-14 13:40:43 +0800

    Update cn-wps.rb

--- Casks/cn-wps.rb
+++ Casks/cn-wps.rb
@@ -1,9 +1,9 @@
 cask 'cn-wps' do
-  version '2.5.0'
-  sha256 '0fe751fbabff8cdec82ebe0a2db6b197727acb2c4604aa10b5c56ef06b58e95a'
+  version '2.6.0'
+  sha256 'ab3936b2dcda6afccb8c1096d43ed8f90d0e11f9a5c8bbf36f0bc89d8882fec3'
 
   # cn-wps was verified as official when first introduced to the cask
-  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version}/WPS_Office_#{version}(4070).dmg"
+  url "http://package.mac.wpscdn.cn/mac_wps_pkg/#{version}/WPS_Office_#{version}(4243).dmg"
   name 'cn-wps'
   homepage 'https://www.wps.cn/'
 
